a67013d902a52fae336c09b99067b693cb8d6f39
Gorjan Zajkovski <Gorjan.Zajkovski@netcetera.com>
2016-05-07 20:35:39 +0200

    - integration of react-native-image-picker - logging of image as base64 encoded string

--- app/config.js
+++ app/config.js
@@ -70,5 +70,30 @@ export default {
       iconName: 'briefcase',
       iconColor: '#090'
     }
-  ]
+  ],
+
+// image picker options
+  imagePickerOptions: {
+    title: 'Choose photo', // specify null or empty string to remove the title
+      cancelButtonTitle: 'Cancel',
+      takePhotoButtonTitle: 'Take Photo...', // specify null or empty string to remove this button
+      chooseFromLibraryButtonTitle: 'Choose from Library...', // specify null or empty string to remove this button
+
+      cameraType: 'back', // 'front' or 'back'
+      mediaType: 'photo', // 'photo' or 'video'
+      videoQuality: 'high', // 'low', 'medium', or 'high'
+      durationLimit: 10, // video recording max time in seconds
+      // carefully test these settings
+      aspectX: 2, // aspectX:aspectY, the cropping image's ratio of width to height
+      aspectY: 1, // aspectX:aspectY, the cropping image's ratio of width to height
+      quality: 0.2, // photos only
+      angle: 0, // photos only
+
+      allowsEditing: false, // Built in functionality to resize/reposition the image
+      noData: false, // photos only - disables the base64 `data` field from being generated (greatly improves performance on large photos)
+      storageOptions: { // if this key is provided, the image will get saved in the documents/pictures directory (rather than a temporary directory)
+      skipBackup: true, // image will NOT be backed up to icloud
+      path: 'images' // will save image at /Documents/images rather than the root
+    }
+  }
 }
--- app/scenes/Map.js
+++ app/scenes/Map.js
@@ -4,13 +4,15 @@ import React, {
   StyleSheet,
   View,
   Text,
-  TouchableOpacity
+  TouchableOpacity,
+  NativeModules
 } from 'react-native';
 
 import MapView from 'react-native-maps';
 import config from '../config';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
+const ImagePickerManager = NativeModules.ImagePickerManager;
 
 export default React.createClass({
   getInitialState() {
@@ -20,8 +22,27 @@ export default React.createClass({
     };
   },
 
+  _takePhoto() {
+    ImagePickerManager.showImagePicker(config.imagePickerOptions, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      }
+      else if (response.error) {
+        console.log('ImagePickerManager Error: ', response.error);
+      }
+
+      else {
+        // You can display the image using either data:
+        const source = {uri: 'data:image/jpeg;base64,' + response.data, isStatic: true};
+        console.log("img in base64: ", source);
+      }
+    });
+  },
+
   render() {
-    const { region, markers } = this.state;
+    const {region, markers} = this.state;
     return (
       <View style={styles.container}>
         <MapView
@@ -38,7 +59,7 @@ export default React.createClass({
                 coordinate={markers[index].coordinate}
                 title={'Checkpoint ' + index}
                 description={'Description ' + index}>
-                <Icon name={marker.iconName} size={30} color={marker.iconColor} />
+                <Icon name={marker.iconName} size={30} color={marker.iconColor}/>
               </MapView.Marker>
             )
           })}
@@ -46,6 +67,9 @@ export default React.createClass({
         <View style={styles.buttonContainer}>
           <TouchableOpacity onPress={() => this.props.pop()} style={[styles.bubble, styles.button]}>
             <Text>Back</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this._takePhoto} style={[styles.bubble, styles.button]}>
+            <Text>Take Photo</Text>
           </TouchableOpacity>
         </View>
       </View>
