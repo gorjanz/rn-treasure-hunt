39bfd7c6a40700e12ba2aa96b446286d59a34b44
Gorjan Zajkovski <Gorjan.Zajkovski@netcetera.com>
2016-05-08 19:31:11 +0200

    added test marker data, to facilitate development. in the live version, the markers will be server driven

---
+++ app/utils/data.js
@@ -0,0 +1,26 @@
+export function getMarkersForTeam(team) {
+  // for testing purposes use example markers
+  // server driven markers => coming soon...
+  return [
+    {
+      coordinate: {
+        latitude: 42.004915,
+        longitude: 21.392972
+      },
+      title: '',
+      description: '',
+      iconName: 'book',
+      iconColor: '#900'
+    },
+    {
+      coordinate: {
+        latitude: 42.003639,
+        longitude: 21.392650
+      },
+      title: '',
+      description: '',
+      iconName: 'camera',
+      iconColor: '#333'
+    }
+  ];
+}
